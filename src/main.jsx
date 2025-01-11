9b8ba8aefa047fb131d71dfe3b090f0735be5d80
amrutgawade <amrutgawade.23@gmail.com>
2025-01-11 15:29:21 +0530

    feat: scroll to top for routing

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,12 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { BrowserRouter as Router } from "react-router-dom";
 import "./index.css";
+import ScrollToTop from "./layout/ScrollToTop.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Router>
+      <ScrollToTop />
       <App />
     </Router>
   </React.StrictMode>
